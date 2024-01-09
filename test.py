70afb1dd874494ce200bc39f6a101b049fd121a1
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-01-10 04:35:02 +0600

    фиксы

--- test.py
+++ test.py
@@ -25,11 +25,11 @@ for id in email_ids:
     result, message_data = mail.uid('fetch', id, '(BODY.PEEK[])')
     raw_email = message_data[0][1].decode("utf-8")
     email_message = email.message_from_string(raw_email)
-
     from_ = email.utils.parseaddr(email_message['From'])[1]
     if from_ not in config['trigger_recipients']:
         continue
 
+    email_data = {}
     if email_message.is_multipart():
         for payload in email_message.get_payload():
             body = payload.get_payload(decode=True)
@@ -44,11 +44,11 @@ for id in email_ids:
         except json.JSONDecodeError:
             continue
 
-    cursor.execute("SELECT * FROM data WHERE name = ? AND email = ?", (email_data['name'], email_data['email']))
+    cursor.execute("SELECT * FROM data WHERE name = ? AND email = ?", (email_data.get('name'), email_data.get('email')))
     if cursor.fetchone() is None:
         continue
 
-    cursor.execute("INSERT INTO data (name, email, phone) VALUES (?, ?, ?)", (email_data['name'], email_data['email'], email_data['phone']))
+    cursor.execute("INSERT INTO data (name, email, phone) VALUES (?, ?, ?)", (email_data.get('name'), email_data.get('email'), email_data.get('phone')))
     conn.commit()
 
 conn.close()
